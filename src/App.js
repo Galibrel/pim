ada62522158d44ec47f2cfdd807654b54df18ad7
gabriel ninin <gabrielninin2011@hotmail.com>
2023-10-12 19:48:35 -0300

    input

--- src/App.js
+++ src/App.js
@@ -4,20 +4,26 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <head>
+      <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'/>
+      </head>
+      <body>
+        <header className="cabecalho">
+          <div className="redes">
+          <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt=""/>
+            <img src="https://www.jokesinhindishayari.com/whatsapp-status-src/whatsapp-no-dp-icon/thumb/no-profile-picture-icon-15.jpg" alt=""/>
+          </div>
+          <div className="menu">
+            <a className="ativo" href="">Holerite</a>
+            <a href="">Funcionários</a>
+            <a href="">Bater Ponto</a>
+            <a className="ultimo" href="">Fale Conosco</a>
+          </div>
+        </header>
+        <div>
+            <h1>A</h1>
+          </div>
+      </body>
     </div>
   );
 }
